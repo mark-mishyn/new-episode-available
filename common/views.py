328a7b3af32da6474703bbcb7acea121b6255760
Mark Mishyn <mark.mishyn@gmail.com>
2017-05-02 15:02:13 +0300

    Added method to display help message

--- common/views.py
+++ common/views.py
@@ -13,15 +13,14 @@ class HandleVkRequestView(APIView):
         'search_tv': {
             'commands': ['search', '-s'],
             'argument_required': True,
-            'argument_name': 'query',
-            'doc': 'Allow to search TV-shows by title',
+            'argument_name': 'search_query',
+            'doc': 'Allow to search TV-shows by title.',
         },
         'add_tv': {
             'commands': ['add', '-a'],
             'argument_required': True,
-            'argument_name': 'number',
-            'doc': 'Allow to add TV-show to your list after search. '
-                   'Usage: add <number_in_last_search_results>',
+            'argument_name': 'number_in_last_search_results',
+            'doc': 'Allow to add TV-show to your list after search.'
         },
         'get_users_tv_shows': {
             'commands': ['list', '-l'],
@@ -36,8 +35,13 @@ class HandleVkRequestView(APIView):
         'remove_tv_show': {
             'commands': ['remove', '-r'],
             'argument_required': True,
-            'argument': 'number or \'all\'',
-            'doc': 'Remove all TV-shows from your list or all TV-shows.',
+            'argument_name': '\'all\' OR number_in_shows_list',
+            'doc': 'Remove TV-show from your list or all TV-shows.',
+        },
+        'get_help': {
+            'commands': ['help', '-h'],
+            'argument_required': False,
+            'doc': 'Show this message.',
         },
     }
 
@@ -60,8 +64,7 @@ class HandleVkRequestView(APIView):
 
         if event_type == 'message_new':
             self.message_new(event_data)
-        else:
-            print('SOME EVENT: ', event_type)
+
         return HttpResponse('ok', status=200)
 
     def message_new(self, event_data):
@@ -74,27 +77,28 @@ class HandleVkRequestView(APIView):
         for handler, params in self.COMMAND_HANDLERS.items():
             if command in params['commands']:
                 if not params['argument_required']:
-                    return getattr(self, handler)()
+                    resp_massage = getattr(self, handler)()
+                    return self._send_message(resp_massage)
 
                 if not arguments:
                     return self._send_message(
-                        'Argument "{}" is required for command "{}"'.format(
-                                params['argument_name'], command))
+                        'Argument "{}" is required for command "{}"'.format(params['argument_name'],
+                                                                            command))
 
-                return getattr(self, handler)(arguments)
+                resp_massage = getattr(self, handler)(arguments)
+                return self._send_message(resp_massage)
 
         all_commands_str = ', '.join(['{} ({})'.format(
                 c['commands'][0], c['commands'][1]) for c in self.COMMAND_HANDLERS.values()])
         return self._send_message(
-                'Invalid command "{}". Available commands are: {}'.format(
-                        command, all_commands_str))
+                'Invalid command "{}". Available commands are: {}'.format(command,
+                                                                          all_commands_str))
 
     def search_tv(self, query):
         serials = self.movie_client.search(query)
 
         if not serials:
-            self._send_message(
-                message='Nothing found with '"{}"', try with another search query.'.format(query))
+            return 'Nothing found with '"{}"', try with another search query.'.format(query)
 
         series_variants_dict = {}
 
@@ -108,14 +112,15 @@ class HandleVkRequestView(APIView):
 
             if tv['original_name'] != tv['name']:
                 resp_message = resp_message.replace('\n', '({})\n'.format(tv['original_name']))
-        self._send_message(resp_message)
 
         TVSeriesVariants.objects.create(vk_user=self.vk_user, variants=series_variants_dict)
+
+        return resp_message
 
     def add_tv(self, number):
         tv_variants = TVSeriesVariants.objects.filter(vk_user=self.vk_user).latest('created')
         if not tv_variants:
-            return self._send_message('You have to search first')
+            return 'You have to search first'
 
         variant_numbers = [int(n) for n in tv_variants.variants.keys()]
         if number not in variant_numbers:
@@ -126,8 +131,7 @@ class HandleVkRequestView(APIView):
             else:
                 valid_numbers = '{}-{}'.format(min(variant_numbers), max(variant_numbers))
 
-            return self._send_message('Invalid number "{}", valid variant are: {}'.format(
-                    number,  valid_numbers))
+            return 'Invalid number "{}", valid variant are: {}'.format(number,  valid_numbers)
 
         tv_series_data = tv_variants.variants[number]
         tv_series, created = TVSeries.objects.get_or_create(
@@ -140,41 +144,52 @@ class HandleVkRequestView(APIView):
             tv_series.save()
 
         self.vk_user.tv_series.add(tv_series)
-        return self._send_message('TV-show "{}" was added to your list.'.format(tv_series.name))
+        return 'TV-show "{}" was added to your list.'.format(tv_series.name)
 
     def get_users_tv_shows(self):
         tv_shows_list_str = self._get_tv_shows_list_str()
-        if tv_shows_list_str:
-            self._send_message(tv_shows_list_str)
-        else:
-            self._send_message(self.EMPTY_TV_SHOWS_MESSAGE)
+        if not tv_shows_list_str:
+            return self.EMPTY_TV_SHOWS_MESSAGE
+
+        return tv_shows_list_str
 
     def update_tv_shows_info(self):
         if not self.vk_user.tv_series.exists():
-            return self._send_message(self.EMPTY_TV_SHOWS_MESSAGE)
+            return self.EMPTY_TV_SHOWS_MESSAGE
 
         self._send_message('Updating information...')
         update_tv_shows_info()
-        self._send_message('Updated list of TV-shows: \n{}'.format(self._get_tv_shows_list_str()))
+        return 'Updated list of TV-shows: \n{}'.format(self._get_tv_shows_list_str())
 
     def remove_tv_show(self, number_or_all):
         if number_or_all == 'all':
             self.vk_user.tv_series.all().delete()
-            return self._send_message('All TV-shows was removed.')
+            return 'All TV-shows was removed.'
 
         try:
             number = int(number_or_all)
         except (ValueError, TypeError):
-            return self._send_message('Invalid number.')
+            return 'Invalid number.'
 
         try:
             tv_show = self.vk_user.tv_series.order_by('id')[number - 1]
             tv_show_name = tv_show.name
             tv_show.delete()
         except IndexError:
-            self._send_message('Invalid number.')
+            return 'Invalid number.'
         else:
-            self._send_message('TV-show "{}" was removed.'.format(tv_show_name))
+            return 'TV-show "{}" was removed.'.format(tv_show_name)
+
+    def get_help(self):
+        resp_message = ''
+
+        for handler in self.COMMAND_HANDLERS.values():
+            resp_message += '({}) {}'.format(handler['commands'][1], handler['commands'][0])
+            if handler['argument_required']:
+                resp_message += ' {}'.format(handler['argument_name'])
+            resp_message += ' - {} \n'.format(handler['doc'])
+
+        return resp_message
 
     def _send_message(self, message):
         return self.vk_client.send_message(user_id=self.user_id, message=message)
@@ -182,5 +197,5 @@ class HandleVkRequestView(APIView):
     def _get_tv_shows_list_str(self):
         tv_shows_list_str = ''
         for i, tv_show in enumerate(self.vk_user.tv_series.order_by('id'), 1):
-            tv_shows_list_str += '{}. {}\n'.format(i, str(tv_show))
+            tv_shows_list_str += '{}. {} \n \n'.format(i, str(tv_show))
         return tv_shows_list_str
